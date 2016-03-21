736e5fd69ec953551ae4eccf0cfffd3cffcfea95
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2016-03-21 12:54:52 +0900

    initial commit

--- test_MapKit/ViewController.swift
+++ test_MapKit/ViewController.swift
@@ -1,25 +1,70 @@
-//
-//  ViewController.swift
-//  test_MapKit
-//
-//  Created by 荒川陸 on 2016/03/21.
-//  Copyright © 2016年 riku_arakawa. All rights reserved.
-//
+
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
-
+// CLLocationManagerDelegateを継承しなければならない
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    // 現在地の位置情報の取得にはCLLocationManagerを使用
+    var lm: CLLocationManager!
+    // 取得した緯度を保持するインスタンス
+    var latitude: CLLocationDegrees!
+    // 取得した経度を保持するインスタンス
+    var longitude: CLLocationDegrees!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // フィールドの初期化
+        lm = CLLocationManager()
+        longitude = CLLocationDegrees()
+        latitude = CLLocationDegrees()
+        
+        // CLLocationManagerをDelegateに指定
+        lm.delegate = self
+        
+        // 位置情報取得の許可を求めるメッセージの表示．必須．
+        lm.requestAlwaysAuthorization()
+        // 位置情報の精度を指定．任意，
+        lm.desiredAccuracy = kCLLocationAccuracyBest
+        // 位置情報取得間隔を指定．指定した値（メートル）移動したら位置情報を更新する．任意．
+        // lm.distanceFilter = 1000
+        
+        // GPSの使用を開始する
+        lm.startUpdatingLocation()
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    /* 位置情報取得成功時に実行される関数 */
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation){
+        // 取得した緯度がnewLocation.coordinate.longitudeに格納されている
+        latitude = newLocation.coordinate.latitude
+        // 取得した経度がnewLocation.coordinate.longitudeに格納されている
+        longitude = newLocation.coordinate.longitude
+        // 取得した緯度・経度をLogに表示
+        NSLog("latiitude: \(latitude) , longitude: \(longitude)")
+        
+        // GPSの使用を停止する．停止しない限りGPSは実行され，指定間隔で更新され続ける．
+        // lm.stopUpdatingLocation()
+        
+        //向きも表示--- あると挙動がうざい ---
+        //mapView.setUserTrackingMode(MKUserTrackingMode.FollowWithHeading, animated: true)
+        //中心に設定--- あると挙動がうざい ---
+        //mapView.setCenterCoordinate(mapView.userLocation.coordinate, animated: false)
+    }
+    
+    /* 位置情報取得失敗時に実行される関数 */
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        // この例ではLogにErrorと表示するだけ．
+        NSLog("Error")
+    }
+    
 }
 
